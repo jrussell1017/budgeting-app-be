fefb5019b0c3a255060ec0301655da9f358155cc
james russell <jamesrussell@pursuit.org>
2022-01-21 12:33:49 -0500

    back end complete and functional

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,9 +1,66 @@
 const express = require("express");
+const transaction = require("../models/transaction.js");
 const transactions = express.Router();
 const transactionsArr = require("../models/transaction.js");
 
+// ROUTES
+// SENDS THE TRANSACTIONS ARRAY
+
+// GET ALL
 transactions.get("/", (req, res) => {
   res.json(transactionsArr);
+});
+
+// GET INDIVIDUAL TRANSACTION
+// ADD ERROR HANDLING
+// PATH PARAM MUST MATCH!!!
+transactions.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if (transactionsArr[id]) {
+    res.json(transactionsArr[id]);
+  } else {
+    res.status(404).json({ error: "Transaction not found." });
+  }
+});
+
+// CREATE TRANSACTION
+// ADD TO END OF ALL TRANSACTIONS
+transactions.post("/", (req, res) => {
+  transactionsArr.push(req.body);
+  res.json(transactionsArr[transactionsArr.length - 1]);
+});
+
+// DELETE
+transactions.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  if (transactionsArr[id]) {
+    res.json(transactionsArr.splice(id, 1));
+  } else {
+    res.status(404).json({ error: "Transaction not found." });
+  }
+});
+
+// UPDATE
+transactions.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!transactionsArr[id]) {
+    res.status(422).json({ error: "Not found." });
+    return;
+  }
+
+  let { date, name, amount, from } = req.body
+  if(date && name && from !== undefined && amount) {
+      transactionsArr[id] = {
+          date,
+          name,
+          amount,
+          from,
+      }
+      res.json(transactionsArr[id])
+  } else {
+      res.status(422).json({ error: "Please provide all fields!"})
+  }
 });
 
 module.exports = transactions;
--- models/transaction.js
+++ models/transaction.js
@@ -3,42 +3,49 @@ module.exports = [
       date: "April 1",
       name: "Groceries",
       amount: -30,
-      from: "Ideal"
+      from: "Ideal",
+      category: "Food"
     },
     {
       date: "April 1",
       name: "Shopping",
       amount: -47,
-      from: "Target"
+      from: "Target",
+      category: "Shopping"
     },
     {
       date: "April 2",
       name: "Income",
       amount: 2000,
-      from: "Google"
+      from: "Google",
+      category: "Income"
     },
     {
       date: "April 5",
       name: "Gas",
       amount: -25,
-      from: "Shell Gas Station"
+      from: "Shell Gas Station",
+      category: "Extra Expense"
     },
     {
       date: "April 6",
       name: "Rent",
       amount: -700,
-      from: "Pike International, LLC"
+      from: "Pike International, LLC",
+      category: "Bill Payment"
     },
     {
       date: "April 8",
       name: "Income",
       amount: 2000,
-      from: "Google"
+      from: "Google",
+      category: "Income"
     },
     {
       date: "April 8",
       name: "Groceries",
       amount: -40,
-      from: "Mr. Kiwi"
+      from: "Mr. Kiwi",
+      category: "Food"
     },
   ];
